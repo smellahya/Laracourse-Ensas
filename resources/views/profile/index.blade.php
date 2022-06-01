c3dcb6d110927e35ccd07d3ce4f9e2d551d2b1f1
mohamed <mohamedettaleb1999@gmail.com>
2022-06-01 05:52:47 +0100

    Final version

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -19,32 +19,32 @@
             <div class="w-full max-w-2xl mx-auto px-6 py-12 flex justify-between">
                 <div>
                     <div class="md:flex md:items-center mb-4">
-                        <div class=" d-inline p-2 m:w-1/3">
+                        <div class="md:w-2/3">
                             <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
                                 Name : 
                             </label>
                         </div>
-                        <div class="d-inline p-2 md:w-2/3">
+                        <div class="md:w-1/3">
                             <span class="block text-gray-600 font-bold">{{ auth()->user()->name }}</span>
                         </div>
                     </div>
                     <div class="md:flex md:items-center mb-4">
-                        <div class="md:w-1/3">
-                            <label class=" text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                        <div class="md:w-2/3">
+                            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
                                 Email :
                             </label>
                         </div>
-                        <div class="md:w-2/3">
+                        <div class="md:w-1/3">
                             <span class="text-gray-600 font-bold">{{ auth()->user()->email }}</span>
                         </div>
                     </div>
                     <div class="md:flex md:items-center mb-4">
-                        <div class="md:w-1/3">
+                        <div class="md:w-2/3">
                             <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
                                 Role :
                             </label>
                         </div>
-                        <div class="md:w-2/3">
+                        <div class="md:w-1/3">
                             <span class="text-gray-600 font-bold">{{ auth()->user()->roles[0]->name ?? '' }}</span>
                         </div>
                     </div>
